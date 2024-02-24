bb58f8bcfaabe8f766fe5636d8fa0b7e0c48bc4e
Zahra <zahraghanepasand@mail.comgit config --global user.email zahraghanepasand@gmail.com>
2024-02-24 15:49:51 +0330

    init repository

---
+++ Js/main.js
@@ -0,0 +1,246 @@
+let novelCategory = [
+    {id: 100, img: 'img/samfoniMordegan.png', title: 'سمفونی مردگان', author: 'عباس معروفی', price: 175000, count: 1},
+    {id: 101, img: 'img/melateEshgh.jpeg', title: 'ملت عشق', author: 'الیف شافاک', price: 240000, count: 1},
+    {id: 102, img: 'img/boofekoor.jpg', title: 'بوف کور', author: 'صادق هدایت', price: 40000, count: 1},
+    {id: 103, img: 'img/midNight.jpg', title: 'کتابخانه نیمه شب', author: 'مک هیگ', price: 195000, count: 1},
+    {id: 104, img: 'img/dokhtari...jpg', title: 'دختری که رهایش کردی', author: 'جوجو مویز', price: 70000, count: 1},
+    {id: 105, img: 'img/vanahade.jpg', title: 'وانهاده', author: 'سیمون دوبووار', price: 68000, count: 1},
+    {id: 106, img: 'img/GhorurVaTaasob.jpg', title: 'غرور و تعصب', author: 'جین آستین', price: 180000, count: 1},
+    {id: 107, img: 'img/chashmhayash.png', title: 'چشمهایش', author: 'بزرگ علوی', price: 145000, count: 1},
+    {id: 108, img: 'img/ove.jpg', title: 'مردی به نام اوه', author: 'فردریک بکمن', price: 239000, count: 1},
+]
+
+let poetryCategory = [
+    {id: 200, img: 'img/fazelNazari.jpg', title: 'گزیدۀ اشعار فاضل نظری', author: 'فاضل نظری', price: 114000, count: 1},
+    {id: 201, img: 'img/forough.jpg', title: 'اولین تپش های عاشقانه قلبم', author: 'فروغ فرخزاد', price: 208000, count: 1},
+    {id: 202, img: 'img/ketab.jpg', title: 'کتاب', author: 'فاضل نظری', price: 66000, count: 1},
+    {id: 203, img: 'img/hafez.jpg', title: 'دیوان حافظ', author: 'حافظ', price: 85000, count: 1},
+    {id: 204, img: 'img/shahriyar.jpg', title: 'دیوان شهریار', author: 'شهریار', price: 203000, count: 1},
+    {id: 205, img: 'img/ayeneh.jpg', title: 'آینه در آینه', author: 'هوشنگ ابتهاج', price: 152000, count: 1},
+    {id: 206, img: 'img/shakspeare.jpg', title: 'غزل های عاشقانه', author: 'ویلیام شکسپیر', price: 134000, count: 1},
+    {id: 207, img: 'img/vojood.jpg', title: 'وجود', author: 'فاضل نظری', price: 73000, count: 1},
+    {id: 208, img: 'img/aknoon.jpg', title: 'اکنون', author: 'فاضل نظری', price: 77000, count: 1},
+]
+
+let psychologyCategory = [
+    {id: 300, img: 'img/takht...jpeg', title: 'تختخوابت را مرتب کن', author: 'ژنرال ویلیام اچ. مک ریون', price: 77000, count: 1},
+    {id: 301, img: 'img/khodetBash.jpg', title: 'خودت باش دختر', author: 'ریچل هالیس', price: 140000, count: 1},
+    {id: 302, img: 'img/asareMorakab.jpg', title: 'اثر مرکب', author: 'دارن هاردی', price: 189000, count: 1},
+    {id: 303, img: 'img/bashgahe5.jpg', title: 'باشگاه پنج صبحی ها', author: 'رابین شارما', price: 195000, count: 1},
+    {id: 304, img: 'img/khodshenasi.jpg', title: 'خودشناسی', author: 'آلن دوباتن', price: 60000, count: 1},
+    {id: 305, img: 'img/khordeAdatha.jpg', title: 'خرده عادت ها', author: 'جیمز کلیر', price: 195000, count: 1},
+    {id: 306, img: 'img/bihadomarz.jpg', title: 'بی حد و مرز', author: 'جیم کوییک', price: 220000, count: 1},
+]
+
+let scientificCategory = [
+    {id: 400, img: 'img/tarikhche...jpg', title: 'تاریخچه زمان', author: 'استیون و. هاوکینگ', price: 160000, count: 1},
+    {id: 401, img: 'img/zhenekhodkhah.jpg', title: 'ژن خودخواه', author: 'ریچارد داکنیز', price: 240000, count: 1},
+    {id: 402, img: 'img/saatsaz.jpg', title: 'ساعت ساز نابینا', author: 'ریچارد داوکینز', price: 50000, count: 1},
+    {id: 403, img: 'img/jahandar.jpg', title: 'جهان در پوست گردو', author: 'استیون هاوکینگ', price: 140000, count: 1},
+    {id: 404, img: 'img/tafakor.jpg', title: 'تفکر علمی ', author: 'رابرت ام. مارتین', price: 197000, count: 1},
+    {id: 405, img: 'img/akhlagh.jpg', title: 'اخلاق', author: 'باروخ اسپینوزا', price: 225000, count: 1},
+    {id: 406, img: 'img/falsafe.jpg', title: 'فلسفه زبان', author: 'ویلیام لایکن', price: 216000, count: 1},
+    {id: 407, img: 'img/darham.jpg', title: 'درهم تنیدگی کوانتومی', author: 'امیر آتسل', price: 174000, count: 1},
+]
+
+let childrenCategory = [
+    {id: 500, img: 'img/pesarak...jpg', title: 'پسرک موش کور روباه و اسب', author: 'چارلی مکسی', price: 239000, count: 1},
+    {id: 501, img: 'img/pastilbanafsh.jpg', title: 'پاستیل های بنفش', author: 'کلی بارن هیل', price: 144000, count: 1},
+    {id: 502, img: 'img/dokhtariKeMah...jpg', title: 'دختری که ماه را نوشید', author: 'کاترین آپلگیت', price: 180000, count: 1},
+    {id: 503, img: 'img/zobaleh.jpg', title: 'زباله نوازان کوچک', author: 'پل ای. رینولدز', price: 25000, count: 1},
+    {id: 504, img: 'img/mehmanenakhande.jpg', title: 'مهمان های ناخوانده', author: 'فریده فرجام', price: 35000, count: 1},
+    {id: 505, img: 'img/tabestan.jpg', title: 'تابستان قوها', author: 'بتسی بایارس', price: 85000, count: 1},
+    {id: 506, img: 'img/haripater.jpg', title: 'هری پاتر و سنگ جادو', author: 'جی. کی. رولینگ', price: 200000, count: 1},
+    {id: 507, img: 'img/dokhtarePorteghali.jpg', title: 'دختر پرتقالی', author: 'یوستاین گاردر', price: 125000, count: 1},
+    {id: 508, img: 'img/moshesarbehava.jpg', title: 'موش سر به هوا', author: 'فرهاد حسن زاده', price: 30000, count: 1}
+]
+
+let $ = document
+let listContainer = $.querySelector('.container')
+let novelContainer = $.querySelector('#novel')
+let poetryContainer = $.querySelector('#poetry')
+let psychologyContainer = $.querySelector('#psychology')
+let scientificContainer = $.querySelector('#scientific')
+let childrenContainer = $.querySelector('#children')
+let basketContainer = $.querySelector('.basket-container')
+let totalPrice = $.querySelector('.total-price')
+
+let userBasket = []
+
+novelCategory.forEach(function (product) {
+    novelContainer.insertAdjacentHTML('beforeend', '<div class="card list-item col-lg-2 col-sm-3 col-4"><img src="' +
+    product.img + '" class="card-img-top item-image" alt="Book Image"><div class="card-body"><h5><a href="product.html?id='
+    + product.id + '" class="card-title">'
+    + product.title + '</a></h5><p class="card-text item-detail">' + product.author + 
+    '</p><spsn class="item-price">' + product.price + ' تومان</spsn><button type="button" class="add-shop p-3" onclick="nAddToBasket(' +
+    product.id + ')"><i class="fa fa-cart-plus fa-2x"></i></button></div></div>')
+})
+
+poetryCategory.forEach(function (product) {
+    poetryContainer.insertAdjacentHTML('beforeend', '<div class="card list-item col-lg-2 col-sm-3 col-4"><img src="' +
+    product.img + '" class="card-img-top item-image" alt="Book Image"><div class="card-body"><h5><a href="product.html?id='
+    + product.id + '" class="card-title">'
+    + product.title + '</a></h5><p class="card-text item-detail">' + product.author + 
+    '</p><spsn class="item-price">' + product.price + ' تومان</spsn><button type="button" class="add-shop p-3" onclick="pAddToBasket(' +
+    product.id + ')"><i class="fa fa-cart-plus fa-2x"></i></button></div></div>')
+})
+
+psychologyCategory.forEach(function (product) {
+    psychologyContainer.insertAdjacentHTML('beforeend', '<div class="card list-item col-lg-2 col-sm-3 col-4"><img src="' +
+    product.img + '" class="card-img-top item-image" alt="Book Image"><div class="card-body"><h5><a href="product.html?id='
+    + product.id + '" class="card-title">'
+    + product.title + '</a></h5><p class="card-text item-detail">' + product.author + 
+    '</p><spsn class="item-price">' + product.price + ' تومان</spsn><button type="button" class="add-shop p-3" onclick="psAddToBasket(' +
+    product.id + ')"><i class="fa fa-cart-plus fa-2x"></i></button></div></div>')
+})
+
+scientificCategory.forEach(function (product) {
+    scientificContainer.insertAdjacentHTML('beforeend', '<div class="card list-item col-lg-2 col-sm-3 col-4"><img src="' +
+    product.img + '" class="card-img-top item-image" alt="Book Image"><div class="card-body"><h5><a href="product.html?id='
+    + product.id + '" class="card-title">'
+    + product.title + '</a></h5><p class="card-text item-detail">' + product.author + 
+    '</p><spsn class="item-price">' + product.price + ' تومان</spsn><button type="button" class="add-shop p-3" onclick="sAddToBasket(' +
+    product.id + ')"><i class="fa fa-cart-plus fa-2x"></i></button></div></div>')
+})
+
+childrenCategory.forEach(function (product) {
+    childrenContainer.insertAdjacentHTML('beforeend', '<div class="card list-item col-lg-2 col-sm-3 col-4"><img src="' +
+    product.img + '" class="card-img-top item-image" alt="Book Image"><div class="card-body"><h5><a href="product.html?id='
+    + product.id + '" class="card-title">'
+    + product.title + '</a></h5><p class="card-text item-detail">' + product.author + 
+    '</p><spsn class="item-price">' + product.price + ' تومان</spsn><button type="button" class="add-shop p-3" onclick="cAddToBasket(' +
+    product.id + ')"><i class="fa fa-cart-plus fa-2x"></i></button></div></div>')
+})
+
+function nAddToBasket (productId) {
+    let isInProduct = userBasket.some(function (product) {
+        return productId === product.id
+    })
+    
+    let mainProduct = novelCategory.find(function (product) {
+        return productId === product.id
+    })
+
+    if (isInProduct) {
+        mainProduct.count += 1
+    } else {
+        userBasket.push(mainProduct)
+    }
+
+    basketProductsGenetator(userBasket)
+}
+
+function pAddToBasket (productId) {
+    let isInProduct = userBasket.some(function (product) {
+        return productId === product.id
+    })
+
+    let mainProduct = poetryCategory.find(function (product) {
+        return productId === product.id
+    })
+
+    if (isInProduct) {
+        mainProduct.count += 1
+    } else {
+        userBasket.push(mainProduct)
+    }
+
+    basketProductsGenetator(userBasket)
+}
+
+function psAddToBasket (productId) {
+    let isInProduct = userBasket.some(function (product) {
+        return productId === product.id
+    })
+
+    let mainProduct = psychologyCategory.find(function (product) {
+        return productId === product.id
+    })
+
+    if (isInProduct) {
+        mainProduct.count += 1
+    } else {
+        userBasket.push(mainProduct)
+    }
+
+    basketProductsGenetator(userBasket)
+}
+
+function sAddToBasket (productId) {
+    let isInProduct = userBasket.some(function (product) {
+        return productId === product.id
+    })
+
+    let mainProduct = scientificCategory.find(function (product) {
+        return productId === product.id
+    })
+
+    if (isInProduct) {
+        mainProduct.count += 1
+    } else {
+        userBasket.push(mainProduct)
+    }
+
+    basketProductsGenetator(userBasket)
+}
+
+function cAddToBasket (productId) {
+    let isInProduct = userBasket.some(function (product) {
+        return productId === product.id
+    })
+
+    let mainProduct = childrenCategory.find(function (product) {
+        return productId === product.id
+    })
+
+    if (isInProduct) {
+        mainProduct.count += 1
+    } else {
+        userBasket.push(mainProduct)
+    }
+
+    basketProductsGenetator(userBasket)
+
+}
+
+function basketProductsGenetator (userBasketArray) {
+    basketContainer.innerHTML = ''
+    userBasketArray.forEach(function (product) {
+        basketContainer.insertAdjacentHTML('beforeend', '<div class="basket-item d-flex p-3 mb-3"><img src="' 
+        + product.img + '" class="basket-item-img h-100"><div class="basket-details d-grid pe-3"><span class="basket-item-title">نام کتاب :'
+        + product.title + '</span><span class="basket-item-details">نویسنده : ' +
+        product.author + '</span><span class="basket-item-price">قیمت : ' + product.price + 
+        ' تومان</span></div><div class="quantity pe-3"><input type="number" value="' + product.count + 
+        '" min="1" max="7" class="basket-quantity text-center" onchange="updateProductCount(' + product.id + ',' + 'value' + 
+        ')"></div><button type="button" class="remove-item ps-4" onclick="removeProduct(' 
+        + product.id + ')"><i class="fa fa-remove position-absolute top-0 start-0 p-2"></i></button></div>')
+    })
+    calcTotalPrice(userBasket)
+}
+
+function removeProduct (productId) {
+    userBasket = userBasket.filter(function (product) {
+        return productId !== product.id
+    })
+    basketProductsGenetator(userBasket)
+    calcTotalPrice(userBasket)
+}
+
+function calcTotalPrice (userBasketArray) {
+    let totalPriceValue = 0
+
+    userBasketArray.forEach(function (product) {
+        totalPriceValue += product.count * product.price
+    })
+    totalPrice.innerHTML = totalPriceValue + ' تومان'
+}
+
+function updateProductCount (productId, newCount) {
+    userBasket.forEach(function (product) {
+        if (productId === product.id) {
+            product.count = newCount
+        }
+    })
+    calcTotalPrice(userBasket)
+}
+
